79190f6fd94b44e7439a906e3f3db17707d11a8c
FoodieBZ <121642769+FoodieBZ@users.noreply.github.com>
2023-02-09 14:30:17 -0500

    toast

--- Challenge.tsx
+++ Challenge.tsx
@@ -34,15 +34,15 @@ export default function Challenge({data} : props) {
         const data = { name, email, password, occupation, state };
         try {
             const response = await fetch("https://frontend-take-home.fetchrewards.com/form", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(data),
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
             });
         
             if (response.status === 201) {
-              console.log("Post succeeded")
+                console.log("Post succeeded")
             } else {
                 console.error("Post failed")
             }
@@ -55,39 +55,25 @@ export default function Challenge({data} : props) {
         <div className='relative h-screen flex overflow-hidden flex-col 
         px-5 justify-evenly items-center mx-auto bg-gradient-to-r from-[#fff5be] to-[#ffef89]'>
             <a href = "https://fetch.com/" target='_blank' rel='noopener noreferrer' className="border-t border-black flex-1 text-center ">
-            <Image src = {fetch_image} alt=""  className = "absolute top-5 left-5 mb:left-1/2 mb:transform mb:-translate-x-1/2  w-[200px] h-[75px]"/>
+                <Image src = {fetch_image} alt=""  className = "absolute top-5 left-5 mb:left-1/2 mb:transform mb:-translate-x-1/2  w-[200px] h-[75px]"/>
             </a>
             <h1 className='mb:text-[20px] mb:tracking-[8px] mb:top-[6rem] absolute top-[5rem] uppercase tracking-[13px] text-[#300d38] font-bold text-[30px]'>
-                    Fetch User Sign-up
+                Fetch User Sign-up
             </h1>
-
-            {/*form start*/}
-            {/*********************/}
             <form onSubmit={handleSubmit(onSubmit)} className='mobile:scale-[.65] absolute top-[7rem] flex flex-col space-y-2 w-fit'>
                 <div className ="flex space-x-2">
-                    {/*name*/}
-                    {/*********************/}
                     <input {...register('name', {required:true})} placeholder = "Name" className="fetchForm"  type="text"/>
                     {errors.name && errors.name.type == "required"}
-
-                    {/*email*/}
-                    {/*********************/}
                     <input {...register('email')} placeholder = "Email" type="email" pattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" title = "Email addresses must have @ followed by a valid email domain." className="fetchForm"/>
                     {errors.email && errors.email.type == "required"}
                 </div>
 
-                {/*password*/}
-                {/*********************/}
                 <input {...register('password')} placeholder = "Password" pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$" title = "Passwords must contain at least 12 letters with a number, letter, uppercase letter and special character consisting of one of these #?!@$%^&*-."className="fetchForm" type="text"/>
                 {errors.password && errors.password.type == "required"}
 
-                {/*password check*/}
-                {/*********************/}
                 <input {...register('password_check')} placeholder = "Verify New Password" className="fetchForm" type="text"/>
                 {errors.password_check && errors.password_check.type == "required"}
 
-                {/*occupation*/}
-                {/*********************/}
                 <select defaultValue=""  {...register('occupation')} className="fetchForm">
                     <option disabled value="" >Select an Occupation</option>
                     {data.occupations.map((occupation, index) => (
@@ -98,8 +84,6 @@ export default function Challenge({data} : props) {
                 </select>
                 {errors.occupation && errors.occupation.message !== "" && errors.occupation.type === "required"}
                 
-                {/*state*/}
-                {/*********************/}
                 <select {...register('state')} defaultValue=""  className="fetchForm">
                     <option disabled value=""> Select a State</option>
                     {data.states.map((state, index) => (
@@ -108,171 +92,75 @@ export default function Challenge({data} : props) {
                         </option>
                     ))}
                 </select>
-                {errors.state && errors.state.message !== "" && errors.state.type === "required"}
                 
-
-                {/*Button on click start*/}
-                {/*********************/}
+                {errors.state && errors.state.message !== "" && errors.state.type === "required"}
                 <button onClick= { () => {
-                getValues("name") === '' ? 
-                toast.error('Please enter in a name.', {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                })
-                :
-                null;
+                    var arr = ["Please fix the following: "]; 
+                    if (getValues("name") === '') { 
+                        arr.push('Please enter in a name. ') 
+                    }
+                    
+                    if (getValues("email") === '') { 
+                        arr.push('Please enter in an email address. ') 
+                    }
 
-                getValues("email") === '' ? 
-                toast.error('Please enter in an email address.', {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                })
-                :
-                null;
+                    if (getValues("occupation") === '') {
+                        arr.push('Please select an occupation. ') 
+                    }
+                
+                    if (getValues("state") === '') {
+                        arr.push('Please select a state. ') 
+                    }
 
-                getValues("occupation") === '' ? 
-                toast.error('Please select an occupation.', {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                })
-                :
-                null;
+                    if (getValues("password") === '') {
+                        arr.push('Please enter in a password. ') 
+                    }
 
-                getValues("state") === '' ? 
-                toast.error('Please select a state.', {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                })
-                :
-                null;
+                    if (getValues("password_check") === '') {
+                        arr.push('Please enter in a verified password. ') 
+                    }
 
-                getValues("password") === '' ? 
-                toast.error('Please enter in a password.', {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                })
-                :
-                null;
+                    if (!getValues("password").match("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$")) {
+                        arr.push('Please enter in a valid password ') 
+                    }
 
-                getValues("password_check") === '' ? 
-                toast.error('Please enter in a verified password.', {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                })
-                :
-                null;
+                    if (getValues("password_check") !== getValues("password")) {
+                        arr.push("Passwords don't match") 
+                    }
 
-                !getValues("password").match("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$") ? 
-                toast.error('Please enter in a valid password.', {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                })
-                :
-                null;
+                    if (!getValues("email").match("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$") && 
+                    getValues("email") !== '') {
+                        arr.push("Please enter in a valid email address.") 
+                    }
 
-                getValues("password_check") !== getValues("password") ? 
-                toast.error("Passwords don't match", {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                })
-                :
-                null;
-
-                !getValues("email").match("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$") && 
-                getValues("email") !== '' ? 
-                toast.error('Please enter in a valid email address.', {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                })
-                :
-                null;
-
-                getValues("password").match("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$") && getValues("email").match("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$") && getValues("password") !== '' && getValues("password_check") !== '' 
-                &&  getValues("email") !== '' && getValues("name") !== '' &&  getValues("password_check") === getValues("password") && getValues("state") !== ''
-                && getValues("occupation") !== '' ? 
-                toast.success('Your Message was sent.', {
-                    position: "top-right",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                })
-                :
-                null;
+                    if (arr.length != 1) {
+                        toast.error(arr.join("\n"), {
+                            position: "top-right",
+                            autoClose: 20000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "colored",
+                        })
+                    }
+                    else {
+                        toast.success('Your Message was sent.', {
+                            position: "top-right",
+                            autoClose: 2000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "colored",
+                        })
+                    }
                 }}
                 type= "submit" className="bg-yellow-400/80 py-5 px-10 rounded-md text-black font-bold hover:bg-yellow-400"> Submit</button>
-                {/*Button on click end*/}
-                {/*********************/}
-
             </form>
-            <ToastContainer 
-                position="top-right"
-                autoClose={2000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="colored"/>
+            <ToastContainer/>
         </div>
     )
 }
